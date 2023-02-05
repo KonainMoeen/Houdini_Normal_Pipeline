2165f9c4986722adf4c338c5e34aa9a4c6aa0b69
Konain <konainmoeen@gmail.com>
2023-02-05 19:29:08 +0500

    Added scatter percent

--- houdini_setup.py
+++ houdini_setup.py
@@ -37,6 +37,7 @@ class HoudiniSetup():
         self.geometry = hou.node('/obj/Geometry')
         
         mask = self.geometry.node('mask_default')
+        detailwrangle = self.geometry.node('detailwrangle_default')
         uv = self.geometry.node('uvproject_default')
         foreach_center_begin =self.geometry.node('foreach_center_begin_default')
         center_position = self.geometry.node('center_position_default')
@@ -59,8 +60,8 @@ class HoudiniSetup():
         blast = self.geometry.node('blast_default')
         out = self.geometry.node('OUT')
         
-        self.default_setup_nodes = [mask, uv, foreach_center_begin, center_position,foreach_center_end, group, merge,connectivity,scatter,randomize_rotations, randomize_scale,sort,attribute_from_pieces,copytopoints, reverse,  foreach_delete_begin, delete_small_parts, foreach_delete_end, attribdelete, transform, blast, out]
-        self.extra_setup_nodes = [mask, uv, foreach_center_begin, delete_small_parts, center_position,foreach_center_end, group]
+        self.default_setup_nodes = [mask, detailwrangle, uv, foreach_center_begin, center_position,foreach_center_end, group, merge,connectivity,scatter,randomize_rotations, randomize_scale,sort,attribute_from_pieces,copytopoints, reverse,  foreach_delete_begin, delete_small_parts, foreach_delete_end, attribdelete, transform, blast, out]
+        self.extra_setup_nodes = [mask, detailwrangle, uv, foreach_center_begin,delete_small_parts, center_position, foreach_center_end, group]
         
     def _lop_set_references(self):
         self.lop = hou.node('/obj/Render')
@@ -97,7 +98,12 @@ class HoudiniSetup():
         #hou.node('/obj/Geometry/center_position_default_' + key).parm('Scale_Multiplier').set(self.settings.get_scale_multiplier()[key])
         hou.node('/obj/Geometry/group_default_' + key).parm('groupname').set(key)
         if scatter_percent > 0:
-            pass
+            img_grey = cv2.imread(current_masks_dict[key][0], cv2.IMREAD_GRAYSCALE)
+            percentage_white_pixels_grey = np.sum(img_grey > 0) / (img_grey.shape[0]*img_grey.shape[1])
+            scatter = int((scatter_percent - float(percentage_white_pixels_grey)) / float(percentage_white_pixels_grey))
+            divider = int((1.01 - scatter_percent) * 100)
+            hou.node('/obj/Geometry/scatter_default_' + key).parm('npts').setExpression(str(scatter) +
+                                                                                        ' * detail("../detailwrangle_default/", "no_of_prims", 0) / ' + str(divider))
         else:
             hou.node('/obj/Geometry/scatter_default_' + key).parm('npts').setExpression(self.settings.get_total_count()[key])
         hou.node('/obj/Geometry/sort_default_' +
