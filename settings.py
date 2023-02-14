0cf6f5b3af729f8c56325313a2e037c3d247fcfb
ML PC <konain.moeen@epicgames.quixel.com>
2023-02-14 20:46:30 +0500

    Updated settings file

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@ class Settings():
     def __init__(self):
         pass
     
-    # Enter in priority # Higher to Lower
+    # Enter in Higher to Lower Priority # Set Priority
     def get_all_classes(self):
-        priority_list = ['green_lichen', 'white_lichen', 'moss', "yellow_lichen"]
+        priority_list = ['green_lichen', 'white_lichen', 'moss']
         priority_list.reverse()
         return priority_list
     
@@ -19,8 +19,10 @@ class Settings():
         return 'exr'
     
     def get_render_resolution(self):
-        return 2048
+        return 1024
     
+    # set instance size to check, if size is equal or lower it will be deleted
+    # [x.y,z]
     def get_delete_small_parts_threshold(self):
         threshold = {}
         threshold['default'] = [0.001,0.001,0]
@@ -30,18 +32,8 @@ class Settings():
 
         return threshold
     
-    # def get_scale_multiplier(self):
-    #     scale = {}
-    #     scale['default'] = 1
-    #     scale['green_lichen'] = 1
-    #     scale['white_lichen'] = 1
-    #     scale['moss'] = 1
-    #     return scale
-    
-    def get_total_count(self): #configure instance count
-        # total - random value between 0 to max amount to subtract
-        # to add new class : 
-        #   no_of_points['class_name'] = 'max number of points - random
+    def get_total_count(self): #configure instance count 
+        #   no_of_points['class_name'] = max number of points - (random value between minimum, maximum)
         no_of_points = {}
         no_of_points['default'] = '100 - fit(rand($F * 12431),0,1,0,50)' 
         no_of_points['green_lichen'] = '200 - fit(rand($F * 25234),0,1,0,100)'
@@ -49,6 +41,32 @@ class Settings():
         no_of_points['moss'] = '100 - fit(rand($F * 12431),0,1,0,50)'
 
         return no_of_points
+
+    # randomizes scale between the minimum and maximum values
+    def get_randomize_scale_values(self):
+        scale_max = {}
+        scale_min  = {}
+        
+        scale_min['default'] = 0.75
+        scale_min['green_lichen'] = 0.75
+        scale_min['white_lichen'] = 0.75
+        scale_min['moss'] = 0.75
+        
+        scale_max['default'] = 1.75
+        scale_max['green_lichen'] = 1.75
+        scale_max['white_lichen'] = 1.75
+        scale_max['moss'] = 1.75
+            
+        scale = [scale_min, scale_max]
+        return scale
+
+    # def get_scale_multiplier(self):
+    #     scale = {}
+    #     scale['default'] = 1
+    #     scale['green_lichen'] = 1
+    #     scale['white_lichen'] = 1
+    #     scale['moss'] = 1
+    #     return scale
     
     #sorting mesh positions for more randomization
     def get_point_sort(self):   # 2=x  3=y  4=z  5=reverse  6=random
@@ -59,21 +77,3 @@ class Settings():
         sort['moss'] = 0
 
         return sort
-    
-    # randomizes scale between the minimum and maximum values
-    def get_randomize_scale_values(self):
-        scale_max = {}
-        scale_min  = {}
-        
-        scale_min['default'] = 1
-        scale_min['green_lichen'] = 1
-        scale_min['white_lichen'] = 1
-        scale_min['moss'] = 1
-        
-        scale_max['default'] = 2
-        scale_max['green_lichen'] = 2
-        scale_max['white_lichen'] = 2
-        scale_max['moss'] = 2
-            
-        scale = [scale_min, scale_max]
-        return scale
